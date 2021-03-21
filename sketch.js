9f8a7e4006b15f1dd460209c39400a2efc4f4d18
Fazliddin1 <76078604+Fazliddin1@users.noreply.github.com>
2021-03-21 20:16:44 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+var block1,block2,block3,block4;
+var ball, edges;
+
+function preload(){
+}
+
+
+function setup(){
+    canvas = createCanvas(800,600);
+
+    block1 = createSprite(0,580,360,30);
+    block1.shapeColor = 'blue';
+
+    block2 = createSprite(295,580,200,30);
+    block2.shapeColor = 'red';
+
+    block3 = createSprite(515,580,200,30);
+    block3.shapeColor = 'green';
+
+    block4 = createSprite(740,580,220,30);
+    block4.shapeColor = 'yellow';
+
+    ball = createSprite(random(20,750),100, 40,40);
+    ball.shapeColor = 'purple';
+    ball.velocityX = 4;
+    ball.velocityY = 9;
+
+}
+
+function draw() {
+    background('black');
+    edges=createEdgeSprites();
+    ball.bounceOff(edges);
+
+    if(block1.isTouching(ball) && ball.bounceOff(block1)){
+        ball.shapeColor = 'blue';
+    }
+
+    if(block2.isTouching(ball)){
+        ball.shapeColor = 'red';
+        ball.velocityX = 0;
+        ball.velocityY = 0;
+    }
+
+    if(block3.isTouching(ball) && ball.bounceOff(block3)){
+        ball.shapeColor = 'green';
+    }
+
+    if(block4.isTouching(ball) && ball.bounceOff(block4)){
+        ball.shapeColor = 'yellow';
+    }
+
+    drawSprites();
+}
